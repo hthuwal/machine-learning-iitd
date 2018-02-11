b6fc8276be0fddc46bb5fdb607412cbe95790971
Harish Thuwal <hthuwal@gmail.com>
2018-02-12 00:59:12 +0530

    Write a function to calculate gradient of L(theta)

--- Assignmet_1/03_logistic_regression.py
+++ Assignmet_1/03_logistic_regression.py
@@ -5,7 +5,22 @@ import my_utils
 def g(z):
     return 1 / (1 + np.exp(-z))
 
-# def gradient_ltheta(x, y, theta):
+
+def gradient_ltheta(x, y, theta):
+    num_examples = x.shape[0]
+    num_features = x.shape[1] - 1
+    gradient = np.zeros([num_features + 1, ])
+
+    for jth_feature in range(0, num_features + 1):
+
+        gradient_wrt_jth_feature = 0.0
+
+        for ith_example in range(0, num_examples):
+            gradient_wrt_jth_feature += (y[ith_example] - g(theta @ x[ith_example])).flatten()[0] * x[ith_example][jth_feature]
+
+        gradient[jth_feature] = gradient_wrt_jth_feature
+
+    return gradient
 
 
 data = my_utils.read_files("logisticX.csv", "logisticY.csv")
@@ -17,3 +32,5 @@ argmin = np.argmin(X, axis=0)
 
 x1_lim = (X[argmin[1]][1] - std[1], X[argmax[1]][1] + std[1])
 x2_lim = (X[argmin[2]][2] - std[2], X[argmax[2]][2] + std[2])
+
+print(gradient_ltheta(X, Y, np.array([1, 2, 3])))
