c9290d16181ac8d349fefa5c72473cb46575aeb6
Harish Thuwal <hthuwal@gmail.com>
2018-02-12 01:08:15 +0530

    Add function to calculaate the hessian of L theta

--- Assignmet_1/03_logistic_regression.py
+++ Assignmet_1/03_logistic_regression.py
@@ -23,6 +23,24 @@ def gradient_ltheta(x, y, theta):
     return gradient
 
 
+def hessian_ltheta(x, y, theta):
+    num_examples = x.shape[0]
+    num_features = x.shape[1] - 1
+
+    hessian = np.zeros([num_features + 1, num_features + 1])
+
+    for ith_feature in range(0, num_features + 1):
+        for jth_feature in range(0, num_features + 1):
+
+            hessian[ith_feature][jth_feature] = 0.0
+
+            for kth_example in range(0, num_examples):
+
+                gz = g(theta @ x[kth_example])
+                hessian[ith_feature][jth_feature] += x[kth_example][ith_feature] * x[kth_example][jth_feature] * (gz - 1) * (gz)
+
+    return hessian
+
 data = my_utils.read_files("logisticX.csv", "logisticY.csv")
 X = data[0]
 Y = data[1]
@@ -34,3 +52,4 @@ x1_lim = (X[argmin[1]][1] - std[1], X[argmax[1]][1] + std[1])
 x2_lim = (X[argmin[2]][2] - std[2], X[argmax[2]][2] + std[2])
 
 print(gradient_ltheta(X, Y, np.array([1, 2, 3])))
+print(hessian_ltheta(X, Y, np.array([1, 2, 3])))
