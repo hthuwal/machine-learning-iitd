51f267ddbe47aa2f0096705883df73ef6a2ceda7
Harish Thuwal <hthuwal@gmail.com>
2018-02-12 09:37:56 +0530

    Add utility function to convert class labels to class category (discrete values)
    Read_files take custom seperator as parameter defaulat values is ','
    
    Read_files changes class labels to class category if elements in class Y are of type string
    ## To make a commit, type your commit message and press CTRL-ENTER. To cancel
    ## the commit, close the window. To sign off on the commit, press CTRL-S.
    ##
    ## You may also reference or close a GitHub issue with this commit.  To do so,
    ## type `#` followed by the `tab` key.  You will be shown a list of issues
    ## related to the current repo.  You may also type `owner/repo#` plus the `tab`
    ## key to reference an issue in a different GitHub repo.
    
     Assignmet_1/my_utils.py | 48 +++++++++++++++++++++++++++++++++++-------------
     1 file changed, 35 insertions(+), 13 deletions(-)
    
    diff --git a/Assignmet_1/my_utils.py b/Assignmet_1/my_utils.py
    index 520cf10..4ab6cb7 100644
    --- a/Assignmet_1/my_utils.py
    +++ b/Assignmet_1/my_utils.py
    @@ -1,24 +1,46 @@
     import numpy as np
     import pandas as pd
    
    +
     def normalize(x):
         mean = np.mean(x, axis=0)
         std = np.std(x, axis=0)
         return np.apply_along_axis(lambda x: (x - mean) / std, 1, x)
    
    -def read_files(x_file, y_file):
    -	X = pd.read_csv("dataset/"+x_file, header=None)
    -	X = X.as_matrix()
    -	X = normalize(X)
    -	temp = X.flatten()
    -	std = np.std(temp)
    -	xlim = (temp[np.argmin(temp)] - std, temp[np.argmax(temp)] + std)
    -	X = np.insert(X, 0, 1.0, axis=1)
    
    +def category_to_discretevalues(Y):
    +    classes = {}
    +    y = []
    +    label = 0
    +    for each in Y:
    +        if each not in classes:
    +            classes[each] = label
    +            label += 1
    +
    +        y.append(classes[each])
    +    return np.array(y)
    +
    +
    +def read_files(x_file, y_file, sep=','):
    +    X = pd.read_csv("dataset/" + x_file, header=None, sep=sep)
    +    X = X.as_matrix()
    +    X = normalize(X)
    +    temp = X.flatten()
    +    std = np.std(temp)
    +    xlim = (temp[np.argmin(temp)] - std, temp[np.argmax(temp)] + std)
    +    X = np.insert(X, 0, 1.0, axis=1)
    +
    +    Y = pd.read_csv("dataset/" + y_file, header=None, sep=sep)
    +    Y = Y.as_matrix().flatten()
    +
    +    if (isinstance(Y[0], str)):
    +        Y = category_to_discretevalues(Y)
    +        std = np.std(Y)
    +        ylim = (Y[np.argmin(Y)] - std, Y[np.argmax(Y)] + std)
    +        return X, Y, xlim, ylim, classes
    +
    +    std = np.std(Y)
    
    -	Y = pd.read_csv("dataset/"+y_file, header=None)
    -	Y = Y.as_matrix().flatten()
    -	std = np.std(Y)
    -	ylim = (Y[np.argmin(Y)] - std, Y[np.argmax(Y)] + std)
    +    ylim = (Y[np.argmin(Y)] - std, Y[np.argmax(Y)] + std)
    
    -	return X, Y, xlim, ylim
    \ No newline at end of file
    +    return X, Y, xlim, ylim

--- Assignmet_1/my_utils.py
+++ Assignmet_1/my_utils.py
@@ -1,24 +1,46 @@
 import numpy as np
 import pandas as pd
 
+
 def normalize(x):
     mean = np.mean(x, axis=0)
     std = np.std(x, axis=0)
     return np.apply_along_axis(lambda x: (x - mean) / std, 1, x)
 
-def read_files(x_file, y_file):
-	X = pd.read_csv("dataset/"+x_file, header=None)
-	X = X.as_matrix()
-	X = normalize(X)
-	temp = X.flatten()
-	std = np.std(temp)
-	xlim = (temp[np.argmin(temp)] - std, temp[np.argmax(temp)] + std)
-	X = np.insert(X, 0, 1.0, axis=1)
+
+def category_to_discretevalues(Y):
+    classes = {}
+    y = []
+    label = 0
+    for each in Y:
+        if each not in classes:
+            classes[each] = label
+            label += 1
+
+        y.append(classes[each])
+    return np.array(y), classes
 
 
-	Y = pd.read_csv("dataset/"+y_file, header=None)
-	Y = Y.as_matrix().flatten()
-	std = np.std(Y)
-	ylim = (Y[np.argmin(Y)] - std, Y[np.argmax(Y)] + std)
+def read_files(x_file, y_file, sep=','):
+    X = pd.read_csv("dataset/" + x_file, header=None, sep=sep)
+    X = X.as_matrix()
+    X = normalize(X)
+    temp = X.flatten()
+    std = np.std(temp)
+    xlim = (temp[np.argmin(temp)] - std, temp[np.argmax(temp)] + std)
+    X = np.insert(X, 0, 1.0, axis=1)
 
-	return X, Y, xlim, ylim
+    Y = pd.read_csv("dataset/" + y_file, header=None, sep=sep)
+    Y = Y.as_matrix().flatten()
+
+    if (isinstance(Y[0], str)):
+        Y, cls_labels = category_to_discretevalues(Y)
+        std = np.std(Y)
+        ylim = (Y[np.argmin(Y)] - std, Y[np.argmax(Y)] + std)
+        return X, Y, xlim, ylim, cls_labels
+
+    std = np.std(Y)
+
+    ylim = (Y[np.argmin(Y)] - std, Y[np.argmax(Y)] + std)
+
+    return X, Y, xlim, ylim
