336d7f7d2b08de87ba27f7eeb273f4a5fc467cfd
Harish Thuwal <hthuwal@gmail.com>
2018-02-12 09:59:39 +0530

    Find the number of elements of each class, and get cls_labels

--- Assignmet_1/04_gda.py
+++ Assignmet_1/04_gda.py
@@ -4,9 +4,13 @@ import numpy as np
 data = my_utils.read_files("q4x.dat", "q4y.dat", sep='\s+')
 X = data[0]
 Y = data[1]
+cls_labels = data[4]
 std = np.std(X, axis=0)
 argmax = np.argmax(X, axis=0)
 argmin = np.argmin(X, axis=0)
 
 x1_lim = (X[argmin[1]][1] - std[1], X[argmax[1]][1] + std[1])
 x2_lim = (X[argmin[2]][2] - std[2], X[argmax[2]][2] + std[2])
+num_yi_is_1 = np.sum(Y) # because rest are zero so sum
+num_yi_is_0 = len(Y) - num_yi_is_1
+print(num_yi_is_0, num_yi_is_1)
