381675ac34cd3f21e652730f1f0f2efc23a363ec
Harish Thuwal <hthuwal@gmail.com>
2018-03-08 10:34:07 +0530

    Train 10C2 classifiers and dump the model

---
+++ Assignment_2/Q2/b_one_vs_one.py
@@ -0,0 +1,49 @@
+from a_pegasos import bgd_pegasos
+import numpy as np
+import pandas as pd
+import pickle
+import os
+
+
+def read_data(file):
+    x = pd.read_csv(file, header=None)
+    y = np.array(x[[784]]).flatten()
+    x = x.drop(columns=[784])
+    x = x.as_matrix()
+    return x, y
+
+
+x_train, y_train = read_data("../mnist/train.csv")
+# x_test, y_test = read_data("../mnist/test.csv")
+
+num_classes = len(set(y_train))
+
+# training each classifier
+retrain = False
+wandbs = None
+if retrain:
+    wandbs = [[() for j in range(num_classes)] for i in range(num_classes)]
+    count = 0
+    for i in range(num_classes):
+        for j in range(num_classes):
+            if(i < j):
+                count += 1
+                print("\nClassifier %d: %d -> 1, %d -> -1\n" % (count, i, j))
+                xc, yc = [], []
+                for x, y in zip(x_train, y_train):
+                    if (y == i):
+                        xc.append(x)
+                        yc.append(1)
+                    elif(y == j):
+                        xc.append(x)
+                        yc.append(-1)
+
+                wandbs[i][j] = bgd_pegasos(xc, yc, 10e-5)
+    with open("model.pickle", "wb") as f:
+        pickle.dump(wandbs, f)
+
+else:
+    with open("model.pickle", "rb") as f:
+        wandbs = pickle.load(f)
+
+print(wandbs[2][1])
