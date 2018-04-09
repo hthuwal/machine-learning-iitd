e5b0c52d2bfc8324774acc04c777a50ee357fd97
Harish Thuwal <hthuwal@gmail.com>
2018-04-09 15:55:04 +0530

    Done I guess

--- Assignment_3/dtree4_5.py
+++ Assignment_3/dtree4_5.py
@@ -3,6 +3,7 @@ import numpy as np
 # from collections import Counter
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 # from tqdm import tqdm
 # import matplotlib.pyplot as plt
@@ -20,27 +21,14 @@ valid_data = rd.preprocess("dataset/dtree_data/valid.csv", binarize=False)
 valid_labels = np.array(valid_data[:, 0])
 valid_data = np.delete(valid_data, 0, 1)
 
-
-decison_tree = DecisionTreeClassifier()
-# decison_tree.fit(train_data, train_labels)
-
-
-# for depth in range(10,50,5):
-#     for split in range(10, 1000, 100):
-#         for leaf in range(10, 1000, 100):
-#             decison_tree = DecisionTreeClassifier(criterion="entropy", max_depth=depth, min_samples_split=split, min_samples_leaf=leaf)
-#             decison_tree.fit(train_data, train_labels)
-#             print(accuracy_score(train_labels, decison_tree.predict(train_data)))
-#             print(accuracy_score(valid_labels, decison_tree.predict(valid_data)))
-#             print(accuracy_score(test_labels, decison_tree.predict(test_data)))
-parameters = {
+parameters_dtree = {
     'criterion': ("gini", "entropy"),
     'max_depth': (None, 10, 20, 30),
     'min_samples_split': (2, 200, 400, 600, 800, 1000),
     'min_samples_leaf': (1, 200, 400, 600, 800, 1000),
 }
-
-# model = GridSearchCV(decison_tree, parameters, verbose=10, n_jobs=2)
+# decison_tree = DecisionTreeClassifier()
+# model = GridSearchCV(decison_tree, parameters_dtree, verbose=10, n_jobs=2)
 # model.fit(train_data, train_labels)
 
 # best model after grid search
@@ -50,7 +38,20 @@ print(accuracy_score(train_labels, decison_tree.predict(train_data)))
 print(accuracy_score(valid_labels, decison_tree.predict(valid_data)))
 print(accuracy_score(test_labels, decison_tree.predict(test_data)))
 
-# print("\nTraining accuracy: %0.2f Validation accuracy: %0.2f Test accuracy: %0.2f" % accuracy(bfs_fast))
+# parameters_rforest = {
+#     'bootstrap': (True, False),
+#     'max_features': (2, 5, 8, 10, 12, 14, 'auto', 'log2', None),
+#     'n_estimators': (20, 25, 30,40, 50, 60),
+# }
 
-# part_c(step=10)
-# pruning()
+# random_forest = RandomForestClassifier(criterion="entropy", max_depth=10, min_samples_leaf=1, min_samples_split=2)
+# model = GridSearchCV(random_forest, parameters_rforest, verbose=5, n_jobs=4)
+# model.fit(train_data, train_labels)
+
+# best model after grid search
+print("RandomForestClassifier")
+random_forest = RandomForestClassifier(criterion="entropy", max_depth=10, min_samples_leaf=1, min_samples_split=2, max_features=12, n_estimators=20, bootstrap=True)
+random_forest.fit(train_data, train_labels)
+print(accuracy_score(train_labels, random_forest.predict(train_data)))
+print(accuracy_score(valid_labels, random_forest.predict(valid_data)))
+print(accuracy_score(test_labels, random_forest.predict(test_data)))
